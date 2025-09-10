d3295ee22f9c85e8ecd332e8bd311826b279ef65
Shivenwattss <shivenwatts@gmail.com>
2025-09-10 12:33:28 +0530

    Create a3q3.cpp

---
+++ a3q3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string expr) {
+    stack<char> s;
+    
+    for (char ch : expr) {
+        
+        if (ch == '(' || ch == '{' || ch == '[') {
+            s.push(ch);
+        }
+        
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (s.empty()) return false; 
+            
+            char top = s.top();
+            s.pop();
+            
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    
+    
+    return s.empty();
+}
+
+int main() {
+    string expression;
+    cout << "Enter an expression: ";
+    getline(cin, expression);
+    
+    if (isBalanced(expression)) {
+        cout << "The expression has balanced parentheses.\n";
+    } else {
+        cout << "The expression does NOT have balanced parentheses.\n";
+    }
+    
+    return 0;
+}
