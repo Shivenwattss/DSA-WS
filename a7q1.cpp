2b126d5905f2d1db2dfbc104423f23501ddcc286
Shivenwattss <shivenwatts@gmail.com>
2025-11-25 17:07:47 +0530

    Create a7q1.cpp

---
+++ a7q1.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+
+
+void display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int curr = arr[i];
+        int prev = i - 1;
+        while (prev >= 0 && arr[prev] > curr) {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr;
+    }
+}
+
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;  
+    }
+}
+
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+int main() {
+    int arr[100], n, choice;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "\nChoose Sorting Algorithm:\n";
+    cout << "1. Selection Sort\n";
+    cout << "2. Insertion Sort\n";
+    cout << "3. Bubble Sort\n";
+    cout << "4. Merge Sort\n";
+    cout << "5. Quick Sort\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            selectionSort(arr, n);
+            cout << "After Selection Sort: ";
+            break;
+        case 2:
+            insertionSort(arr, n);
+            cout << "After Insertion Sort: ";
+            break;
+        case 3:
+            bubbleSort(arr, n);
+            cout << "After Bubble Sort: ";
+            break;
+        case 4:
+            mergeSort(arr, 0, n - 1);
+            cout << "After Merge Sort: ";
+            break;
+        case 5:
+            quickSort(arr, 0, n - 1);
+            cout << "After Quick Sort: ";
+            break;
+        default:
+            cout << "Invalid choice!";
+            return 0;
+    }
+
+    display(arr, n);
+    return 0;
+}
